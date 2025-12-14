5147582561820aa758c560497d3bc4b006eca2c1
Adz Dzaka <cuylearn@gmail.com>
2025-12-14 18:12:04 +0700

    Membuka chart dan beberapa elemen dashboard

--- app/query/route.ts
+++ app/query/route.ts
@@ -4,11 +4,12 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 async function listInvoices() {
 	const data = await sql`
-    SELECT invoices.amount, customers.name
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE invoices.amount = 666;
-  `;
+    SELECT
+        id,
+        name
+      FROM customers
+      ORDER BY name ASC
+    `;
 
 	return data;
 }
